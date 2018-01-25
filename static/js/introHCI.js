93b6d87082b35540f2eb02727afbfaffbb16a1ed
JarninFang <jarnin.fang@servicenow.com>
2018-01-25 01:23:51 -0800

    added Js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hello world!");
+    $("#testjs").text("Please wait...");
+    $(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
@@ -21,5 +23,10 @@ function initializePage() {
 function projectClick(e) {
   e.preventDefault();
   var containingProject = $(this).closest(".project");
-  containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+  var description = $(containingProject).find(".project-description");
+  if (description.length == 0) {
+     $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+  } else {
+    $(containingProject).find(".project-description").fadeToggle();
+  }
 }
